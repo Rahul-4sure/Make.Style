998148c4e547e7ba9b95e155c14a181d8cd85472
Manish kumar <75328802+manishkumar123456@users.noreply.github.com>
2025-03-31 21:43:33 +0530

    Update Logo.tsx

--- Make.Style/src/components/Logo.tsx
+++ Make.Style/src/components/Logo.tsx
@@ -6,7 +6,6 @@ export function Logo() {
     <div className="flex items-center gap-2">
       <div className="w-8 h-8 bg-indigo-600 rounded-lg"></div>
       <h1 className="text-xl font-bold">Make.Style</h1>
-
       <span className="text-xs bg-[#2b2929] px-2 py-1 rounded hover:bg-sky-700 shadow hover:scale-[1.1]">v0.4 Beta</span>
 
    
